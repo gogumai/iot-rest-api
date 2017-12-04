357782afe0e2c5f230d93116affed7c963db23d8
gogumai <gonzalo.gumai@gmail.com>
2017-12-04 12:47:51 -0300

    Koa router hello-world

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const Koa = require('koa');
+var Koa = require('koa');
+var compress = require('koa-compress');
+var logger = require('koa-logger');
+
+const indexRoutes = require('./src/routes');
+
 const app = new Koa();
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
+// Environment
+var env = process.env.NODE_ENV || 'development';
 
-app.listen(3000);
+// Port
+const PORT = process.env.PORT || 1337;
+
+// Logger
+app.use(logger());
+
+// Compress
+app.use(compress());
+
+
+app.use(indexRoutes.routes());
+
+app.listen(PORT);
+console.log(`listening on port ${PORT}`);
